54a358268cd4f2a25c95e02cfcbc461c01842567
WeizheGuo <karlguo0505@yahoo.com>
2019-05-12 11:02:24 -0400

    complete

--- 2019/receiver.py
+++ 2019/receiver.py
@@ -41,7 +41,100 @@ class BogoReceiver(Receiver):
             except socket.timeout:
                 sys.exit()
 
+class MyReceiver(Receiver):
+    # Initialization all variables
+    rcv_array = bytearray([0,0,0,0])
+    last_acknum = -1 
+    ACK_backup = bytearray([0,0,0])
+    resend = True
+    num_duplicates = 0
+
+
+    def __init__(self, timeout = 0.1):
+        super(MyReceiver, self).__init__()
+        self.timeout = timeout
+        self.simulator.rcvr_socket.settimeout(self.timeout)
+
+    def receive(self):
+        while True:
+            try:
+                # Receive data and check the timeout 
+                self.rcv_array = self.simulator.u_receive()
+                if self.timeout > 0.1:
+                    self.timeout += -(0.1)
+                    self.num_duplicates = 0
+
+                # Send the ACK 
+                self.send()
+
+            # If timeout, resend the the back-up ack
+            except socket.timeout:
+                self.resend = True
+                self.simulator.u_send(self.ACK_backup)
+                self.num_duplicates += 1
+                # Handle the case of too many dups
+                if self.num_duplicates >= 3:
+                    self.num_duplicates = 0
+                    self.timeout *= 2
+                    self.simulator.rcvr_socket.settimeout(self.timeout)
+                    if self.timeout > 5:
+                        exit()
+                        
+    def send(self):
+        # Create an ACK to be sent
+        ACK_seg = mysegment()
+        ACK_success = ACK_seg.ack(self.rcv_array, self.last_acknum)
+        if ACK_success:
+            self.last_acknum = ACK_seg.ACK_num
+        if ACK_seg.ACK_num < 0:
+            ACK_seg.ACK_num = 0 # Might be set back to -1
+        ACK_seg.check_sum = ACK_seg.checkSum()
+        rcv_array = bytearray([ACK_seg.check_sum, ACK_seg.ACK_num])
+        ACK_backup = rcv_array
+        self.simulator.u_send(rcv_array)
+
+class mysegment(object):
+
+    def __init__(self, check_sum = 0, seq_num = 0, ACK_num = 0, data = []):
+        self.check_sum = check_sum
+        self.seq_num = seq_num
+        self.ACK_num = ACK_num
+        self.data = data
+         
+    # receiver's segment checksum will just be the ack number
+    def checkSum(self):        
+        return self.ACK_num
+        
+
+    # Checks the checksum (i.e. the ACK number in this case)
+    def checkACK(self,data):
+        # Invert all the bits in the first row of the data array (i.e. the checksum row)         
+        check_sum_val =~ data[0]    
+        for i in xrange(1,len(data)):
+            # XOR all elements of data, similar for sender
+            check_sum_val ^= data[i]
+        if check_sum_val ==- 1:
+            # -1 means being valid
+            return True
+        else:
+            return False
+    
+    # Checks if the ACK is valid 
+    def ack(self, data, last_acknum):
+        we_going_to_standings = self.checkACK(data)
+        if we_going_to_standings:
+            self.ACK_num = (data[2] + len(data[3:])) % 256
+            if data[2] == last_acknum or last_acknum == -1:
+                sys.stdout.write("{}".format(data[3:]))
+                sys.stdout.flush()
+                return True
+        else:
+            pass
+
+        return False
+
 if __name__ == "__main__":
     # test out BogoReceiver
-    rcvr = BogoReceiver()
+    #rcvr = BogoReceiver()
+    rcvr = MyReceiver()
     rcvr.receive()
--- 2019/sender.py
+++ 2019/sender.py
@@ -7,6 +7,11 @@ import channelsimulator
 import utils
 import sys
 
+import random
+import math
+
+MAX_Num_SEQ = 256
+
 class Sender(object):
 
     def __init__(self, inbound_port=50006, outbound_port=50005, timeout=10, debug_level=logging.INFO):
@@ -40,9 +45,161 @@ class BogoSender(Sender):
             except socket.timeout:
                 pass
 
+class MySender(Sender):
+    # Setting up some preliminary parameters
+    Data_file = 0
+    Max_Seg = 250  # Maximum Segment Size
+    Num_segment = 0 # Segment Number
+    partition = 0 # Number of partition 
+    start = 0 # Start of partition
+    end = Max_Seg  # End of partition
+    Num_seq = random.randint(0, 255) # Pick a random sequence number to begin with
+    
+    # Setting up data buffer
+    data_buffer = bytearray(MAX_Num_SEQ)
+    Buffer_start = Num_seq
+    Buffer_end = Num_seq
+
+    num_duplicates = 0
+    is_sent = False
+    resend = False
+
+    # Initialization
+    def __init__(self, DATA, timeout = 0.1):
+        super(MySender, self).__init__()
+        self.Data_file = DATA
+        self.timeout = timeout
+        self.simulator.sndr_socket.settimeout(self.timeout)
+        self.Num_segment = int(math.ceil(len(self.Data_file)/float(self.Max_Seg)))
+
+    def send(self, data):
+        self.logger.info("Sending on port: {} and waiting for ACK on port: {}".format(self.outbound_port, self.inbound_port))
+
+        for s in self.splitSegment(self.Data_file, self.Max_Seg, self.partition):
+            try:
+
+                # two cases are handled: 1st sending data and timeout
+
+                if not self.resend:
+                    # Set up the format for sending segment
+                    # The segment is in this format [ CHECKSUM | Num_SEQ | ACK_NUM | DATA]
+                    seg = MySegment(Num_seq = 0, ack_num = 0, check_sum = 0, data = s)
+                    seg.Num_seq = MySegment.seqNum(self, self.Num_seq, self.Max_Seg)
+                    self.Num_seq = seg.Num_seq
+                    seg.ack_num = 0
+
+                    # The actual format for sending data
+                    send_array = bytearray([seg.check_sum, seg.ack_num, seg.Num_seq])
+                    send_array += s
+
+                    # Define check_sum associated with data 
+                    seg.check_sum = MySegment.checkSum(self, send_array)
+                    send_array[0] = seg.check_sum
+
+                    # Send the data       
+                    self.simulator.u_send(send_array) 
+
+                # Receiving from the receiver and figuring out what to do
+                while True:
+                    rcv_array = self.simulator.u_receive()
+
+                    # ACK corrupted, resend data
+                    if self.checkACK(rcv_array):
+                        # Sequence number is correct 
+                        if rcv_array[1] == self.Num_seq:
+                            self.is_sent = True
+                            self.simulator.u_send(send_array)  
+
+                        # If ACK for subsequent segment comes in, know that the prev segment was also received
+                        elif rcv_array[1] == (self.Num_seq + len(s)) % MAX_Num_SEQ: 
+                            self.num_duplicates = 0
+                            if self.timeout > 0.1:
+                                self.timeout -= 0.1
+                            self.simulator.sndr_socket.settimeout(self.timeout)
+                            self.resend = False
+                            break
+
+                        # There was an error, resend     
+                        else: 
+                            self.simulator.u_send(send_array) 
+                    
+                    # ACK corrupted, resend data
+                    else:
+                        self.simulator.u_send(send_array) 
+                        self.num_duplicates += 1
+                        
+                        # If the package was sent and there are three duplicate ACKs, wait longer
+                        if self.num_duplicates == 3 and self.is_sent:
+                            self.timeout *= 2
+                            self.simulator.sndr_socket.settimeout(self.timeout) 
+                            self.num_duplicates = 0
+                            if self.timeout > 5:
+                                print("Timeout")
+                                exit()
+
+            # If timeout, resend the data
+            except socket.timeout:
+                self.resend = True
+                self.simulator.u_send(send_array)
+                self.num_duplicates += 1
+                if self.num_duplicates >= 3:
+                    self.num_duplicates = 0
+                    self.timeout *= 2
+                    self.simulator.sndr_socket.settimeout(self.timeout)
+                    if self.timeout > 5:
+                        print("Timeout")
+                        exit()                                           
+
+
+    # Checks the checksum of the receiver ACK
+    def checkACK(self, data):
+        # checksum val is the inversion of the 1st element in data
+        check_sum_val = ~data[0]
+        for i in xrange(1, len(data)):
+            # XOR of all elements in data  
+            check_sum_val ^= data[i]
+        if check_sum_val == -1: 
+            return True 
+        else:
+            return False
+
+    # Split up the segment into smaller segements
+    def splitSegment(self, data, Max_Seg, partition):
+        for i in range(self.Num_segment):
+            partition += 1
+            yield data[self.start:self.end]
+            self.start = self.start + Max_Seg # New start of partition
+            self.end = self.end + Max_Seg # New end of partition 
+
+
+
+# The data is in this segment, along with error checking stuff 
+class MySegment(object):
+
+    def __init__(self, check_sum = 0, Num_seq = 0, ack_num = 0, data = []):
+        self.check_sum = check_sum
+        self.ack_num = ack_num
+        self.Num_seq = Num_seq
+        self.data = data
+
+    @staticmethod
+    def seqNum(self, prev_Num_seq, Max_Seg):
+        return (prev_Num_seq + Max_Seg) % MAX_Num_SEQ
+
+    # Make data into a byte array and then do the checksum 
+    @staticmethod
+    def checkSum(self, data):
+        data_array = bytearray(data)
+        check_sum_val = 0
+        for i in xrange(len(data_array)):
+            check_sum_val ^= data_array[i]
+        return check_sum_val
+
+
 
 if __name__ == "__main__":
     # test out BogoSender
     DATA = bytearray(sys.stdin.read())
-    sndr = BogoSender()
+    #sndr = BogoSender()
+    sndr = MySender(DATA)
     sndr.send(DATA)
